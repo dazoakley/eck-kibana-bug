800ba2120324ada5c554bc06b53f7d6ab3711902
Darren Oakley <darren.oakley@johnlewis.co.uk>
2019-07-18 13:49:36 +0100

    Add some sleeps

--- run_me.sh
+++ run_me.sh
@@ -4,9 +4,11 @@ minikube start --cpus 4 --memory 8192
 minikube addons enable ingress
 
 kubectl apply -f all-in-one.yaml
+sleep 5
 kubectl apply -f es.yaml
 kubectl apply -f kibana.yaml
 kubectl apply -f ingress.yaml
+sleep 5
 
 MINIKUBE_IP=$(minikube ip)
 PASSWORD=$(kubectl get secret quickstart-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode)
